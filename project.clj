3fff04a92fd0d459da261450f304cd34919b9288
Sanshiro Yoshida <halcat0x15a@gmail.com>
2012-12-04 16:56:23 +0900

    fix jnlp

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :plugins [[lein-cljsbuild "0.2.9"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.logic "0.7.5"]
-                 [org.clojure/tools.nrepl "0.2.0-beta10"]]
-  :resource-paths ["/usr/lib/jvm/javafx-sdk/rt/lib/jfxrt.jar"]
+                 [org.clojure/tools.nrepl "0.2.0-beta10"]
+                 [com.oracle/javafx-runtime "2.2"]]
   :cljsbuild {:builds [{:source-path "src-cljs"}]
               :crossovers [tic-tac-toe.core
                            tic-tac-toe.game]
